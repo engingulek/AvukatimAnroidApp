b1aff30a9b5658bf20d5293cb0b19030492b048b
Engin Gülek <engingulek0@gmail.com>
2022-04-21 14:59:08 +0300

    CreateMeetingUi

--- Avukatım/app/src/main/java/com/example/test/fragment/LawyerDetailsFragment.kt
+++ Avukatım/app/src/main/java/com/example/test/fragment/LawyerDetailsFragment.kt
@@ -1,11 +1,15 @@
 package com.example.test.fragment
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
@@ -38,9 +42,103 @@ class LawyerDetailsFragment : Fragment() {
             Log.e("Tarih",it[0].commentDate)
         })
 
+        design.bttnMeetingOne.setOnClickListener {
+            design.randevu.visibility = View.VISIBLE
 
+        }
+
+
+        design.imageViewClose.setOnClickListener {
+            design.randevu.visibility = View.GONE
+        }
+
+        stateMeetin()
 
         return design.root
+    }
+
+
+    fun stateMeetin(){
+        var stateChipOne = false
+        var stateChipTwo = false
+        var stateChipThree = false
+        var stateChipFour = false
+        var stateChipOnee = true
+
+
+            // başkası tarafından randevu alınmış
+            design.chipOneC.setBackgroundResource(R.drawable.test_red)
+
+
+        design.chipOne.setOnClickListener{
+            if (stateChipOnee == true) {
+                Toast.makeText(requireContext(),"Başkası Tarından alınmıştır",Toast.LENGTH_SHORT).show()
+
+            }
+
+            else {
+
+                if(stateChipOne == false) {
+                    design.chipOneC.setBackgroundResource(R.drawable.test_red)
+                    stateChipOne = true
+
+                }
+                else {
+                    design.chipOneC.setBackgroundResource(R.drawable.test)
+                    stateChipOne = false
+
+                }
+
+            }
+
+
+
+        }
+
+        design.chipTwo.setOnClickListener{
+            if(stateChipTwo == false) {
+                design.chipTwoC.setBackgroundResource(R.drawable.test_red)
+                stateChipTwo = true
+
+            }
+            else {
+                design.chipTwoC.setBackgroundResource(R.drawable.test)
+                stateChipTwo = false
+
+            }
+
+
+        }
+
+        design.chipThree.setOnClickListener{
+            if(stateChipThree == false) {
+                design.chipThreeC.setBackgroundResource(R.drawable.test_red)
+                stateChipThree = true
+
+            }
+            else {
+                design.chipThreeC.setBackgroundResource(R.drawable.test)
+                stateChipThree = false
+
+            }
+
+
+        }
+
+        design.chipFour.setOnClickListener{
+            if(stateChipFour == false) {
+                design.chipFourC.setBackgroundResource(R.drawable.test_red)
+                stateChipFour = true
+
+            }
+            else {
+                design.chipFourC.setBackgroundResource(R.drawable.test)
+                stateChipFour = false
+
+            }
+
+
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
